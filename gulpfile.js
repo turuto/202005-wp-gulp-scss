d2a1ac8233b65f02feafe369fc31b7e6c5864f1a
Alberto García Ariza <turuto@users.noreply.github.com>
2020-07-30 11:07:00 +0200

    add a little wait to prevent errors of "file unreadable"

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,64 @@
 var gulp = require('gulp'),
-	gulpif = require('gulp-if'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifyCss = require('gulp-minify-css'),
-	notify = require('gulp-notify'),
-	plumber = require('gulp-plumber'),
-	rename = require("gulp-rename");
+    gulpif = require('gulp-if'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCss = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    wait = require('gulp-wait'),
+    rename = require("gulp-rename");
 
 /*CONFIG*/
 var config = {};
-config.activeTheme = 'spcp';
+config.activeTheme = 'styles';
 config.develop = true;
-config.srcSCSSFolder = `../${config.activeTheme}/scss`;
+config.srcSCSSFolder = `./scss`;
 config.mainSCSSFile = `${config.srcSCSSFolder}/main.scss`;
-config.distCSSFolder = `../${config.activeTheme}`;
+config.mainSCSSFile_dev = `${config.srcSCSSFolder}/main.dev.scss`;
+config.distCSSFolder = `./${config.activeTheme}`;
 
 var onError = function (err) {
-	notify.onError({
-		title: 'Gulp',
-		subtitle: 'Failure!',
-		message: 'Error: <%= error.message %>',
-		sound: 'Beep'
-	})(err);
+    notify.onError({
+        title: 'Gulp',
+        subtitle: 'Failure!',
+        message: 'Error: <%= error.message %>',
+        sound: 'Beep'
+    })(err);
 
-	this.emit('end');
+    this.emit('end');
 };
 
 /*SUBTASK*/
 gulp.task('css', () => {
-	console.log(`Executing CSS task on '${config.distCSSFolder}' - isDevelop: ${config.develop}`);
-	return new Promise((resolve, reject) => {
-		return gulp.src(config.mainSCSSFile)
-			.pipe(plumber({
-				errorHandler: onError
-			}))
-			.pipe(sass())
-			.pipe(autoprefixer())
-			.pipe(gulpif((config.develop === false), minifyCss()))
-			.pipe(rename('style.css'))
-			.pipe(gulp.dest(config.distCSSFolder))
-			.on('error', reject)
-			.on('end', resolve);
-	});
+    console.log(`Executing CSS task on '${config.distCSSFolder}' - isDevelop: ${config.develop}`);
+    let entryFile = config.develop ? config.mainSCSSFile_dev : config.mainSCSSFile;
+    // if (config.develop) {
+    //     entryFile = config.mainSCSSFile_dev;
+    // } else {
+    //     entryFile = config.mainSCSSFile;
+    // }
+    return new Promise((resolve, reject) => {
+        return gulp.src(entryFile)
+            .pipe(plumber({
+                errorHandler: onError
+            }))
+            .pipe(wait(200))
+            .pipe(sass())
+            .pipe(autoprefixer())
+            .pipe(gulpif((config.develop === false), minifyCss()))
+            .pipe(rename('style.css'))
+            .pipe(gulp.dest(config.distCSSFolder))
+            .on('error', reject)
+            .on('end', resolve);
+    });
 });
 
 setDevelop = async () => {
-	config.develop = true;
+    config.develop = true;
 };
 
 setProd = async () => {
-	config.develop = false;
+    config.develop = false;
 };
 
 gulp.task('dev', gulp.series(setDevelop, 'css'));
@@ -57,8 +66,8 @@ gulp.task('dev', gulp.series(setDevelop, 'css'));
 gulp.task('prod', gulp.series(setProd, 'css'));
 
 function watch() {
-	const folder = `${config.srcSCSSFolder}/**/*.scss`;
-	gulp.watch(folder, gulp.parallel('css'));
+    const folder = `${config.srcSCSSFolder}/**/*.scss`;
+    gulp.watch(folder, gulp.parallel('css'));
 }
 
 gulp.task('watch', gulp.series('css', watch));
